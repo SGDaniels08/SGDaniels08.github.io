a1b69f8b2d8737f4c771142cb8b55f53e2b85b75
Shawn Daniels <SGDaniels08@gmail.com>
2020-07-29 21:44:40 -0400

    Caption toggle functional but a little ugly

--- portfolio.js
+++ portfolio.js
@@ -1,23 +1,16 @@
 
-const latinCaption = document.createElement("figcaption");
-latinCaption.classList.add("aboutme__Latincaption");
-latinCaption.innerHTML = "Forsan et haec olim meminisse iuvabit."
-
-const englishCaption = document.createElement("figcaption");
-englishCaption.classList.add("aboutme__Latincaption");
-englishCaption.innerHTML = "Perhaps one day it will help to remember even these things."
-
-const captionAttribution = document.createElement("figcaption");
-captionAttribution.classList.add("aboutme__captionattribution");
-captionAttribution.innerHTML = "- Aeneid 1.206";
+const latinCaption = document.querySelector(".aboutme__Latincaption");
+const englishCaption = document.querySelector(".aboutme__Englishcaption");
 
 const aboutme = document.querySelector(".aboutme__figure");
 
 aboutme.addEventListener("mouseover", ()=> {
     console.log("inside element");
-    aboutme.appendChild(englishCaption);    
+    latinCaption.classList.toggle("hidden");
+    englishCaption.classList.toggle("hidden");   
 });
 aboutme.addEventListener("mouseout", ()=> {
     console.log("outside element");
-    aboutme.appendChild(latinCaption);
+    latinCaption.classList.toggle("hidden");
+    englishCaption.classList.toggle("hidden"); 
 });
