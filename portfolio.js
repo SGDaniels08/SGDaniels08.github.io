7f73ddf89619e6a96c0136a3c1b6b21e1f543427
Shawn Daniels <SGDaniels08@gmail.com>
2020-09-07 22:28:05 -0400

    Header and landing responsive

--- portfolio.js
+++ portfolio.js
@@ -1,8 +1,8 @@
 
-const latinCaption = document.querySelector(".aboutme__Latincaption");
-const englishCaption = document.querySelector(".aboutme__Englishcaption");
+const latinCaption = document.querySelector(".about-me__Latincaption");
+const englishCaption = document.querySelector(".about-me__Englishcaption");
 
-const aboutme = document.querySelector(".aboutme__figure");
+const aboutme = document.querySelector(".about-me__figure");
 
 aboutme.addEventListener("mouseover", ()=> {
     console.log("inside element");
