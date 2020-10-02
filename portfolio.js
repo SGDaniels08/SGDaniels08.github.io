23770499dbcc521d8b11abfdb2beaf3cf19762e6
Shawn Daniels <SGDaniels08@gmail.com>
2020-10-01 23:33:52 -0400

    Working on fonts

--- portfolio.js
+++ portfolio.js
@@ -96,15 +96,26 @@ aboutme.addEventListener("touchstart", ()=> {
 
 // Dark Mode
 
-const body = document.querySelector("#body");
+const body = document.querySelector(".body");
+const header = document.querySelector(".header");
+const header__listItemAnchor = document.querySelector(".header__listItemAnchor");
+const header__titleAnchor = document.querySelector(".header__titleAnchor");
 const spacer1 = document.querySelector("#spacer1");
 const spacer2 = document.querySelector("#spacer2");
 const spacer3 = document.querySelector("#spacer3");
 const spacer4 = document.querySelector("#spacer4");
+const footer = document.querySelector(".footer");
 
 const darkmodeButton = document.querySelector("#darkmode-button");
 
 darkmodeButton.addEventListener("click", ()=> {
     body.classList.toggle("body-dark");
+    header.classList.toggle("header-dark");
+    header__listItemAnchor.classList.toggle("header__listItemAnchor-dark");
+    header__titleAnchor.classList.toggle("header__titleAnchor-dark");
     spacer1.classList.toggle("spacer1-dark");
+    spacer2.classList.toggle("spacer2-dark");
+    spacer3.classList.toggle("spacer3-dark");
+    spacer4.classList.toggle("spacer4-dark");
+    footer.classList.toggle("footer-dark");
 });
